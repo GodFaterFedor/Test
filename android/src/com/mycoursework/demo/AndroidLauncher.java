f3e9158d2e80cc04eeaa5c8eefcd883fd0d0309e
GodFaterFedor <fedorotec@gmail.com>
2017-02-20 23:36:46 +0300

    test

---
+++ android/src/com/mycoursework/demo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mycoursework.demo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mycoursework.demo.CurlingDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CurlingDemo(), config);
+	}
+}
